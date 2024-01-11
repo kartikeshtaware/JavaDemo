26db56de33aa079014f03d9ede5615f1343f62a4
Kartikesh <2023bit504@sggs.ac.in>
2024-01-11 12:04:58 +0530

    This is addition function

---
+++ program codes/addition.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+class addition
+{
+   static int add(int a, int b)
+   {
+      int c=a;
+      int d = b;
+      
+      return c+d;
+    
+   }
+   public static void main(String args[])
+   {
+     System.out.println( add(20,30));
+   }
+}
